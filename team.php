a1f303154561fc7a6ece7a6b2dd24a667c8e17dc
ryuto0024 <2220288@ecc.ac.jp>
2023-04-20 10:08:13 +0900

    テスト

---
+++ team.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/team.css" rel="stylesheet">
+    <title>安否情報表示機能</title>
+</head>
+
+<body>
+
+    <h2>安否情報</h2>
+    <h3>所属部署</h3>
+    <div class="tab">
+        <table>
+            <thead>
+                <tr>
+                    <th>名前</th>
+                    <th>安否情報</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>あ</th>
+                    <th>A</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="a">
+        <a href="home.html">戻る</a>
+    </div>
+</body>
+
+</html>
